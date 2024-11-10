16a259e816bcd0fa1ba959a3ffeb51d82dfa248d
abusch <abusch@umd.edu>
2024-11-09 22:04:12 -0500

    added rive stuff

--- Assets/shop_script.cs
+++ Assets/shop_script.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-
-
 public class shop_script : MonoBehaviour
 {
     
@@ -40,6 +38,7 @@ public class shop_script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         gs = FindObjectOfType<gamestate_script>();
     }
 
