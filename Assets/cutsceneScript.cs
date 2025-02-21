33f46f23c08b4e8961457d09215b2fab666d3d9e
kyafuso7 <70420682+kyafuso7@users.noreply.github.com>
2025-02-21 18:48:13 -0500

    Added Visual Novel text display

--- Assets/cutsceneScript.cs
+++ Assets/cutsceneScript.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class cutsceneScript : MonoBehaviour
 {
+     List<string> cutsceneDialog = new List<string>()
+    {
+        "",
+        "testString1",
+        "testString2",
+       
+    };
+    public TextMeshProUGUI DialogText;
+    int DialogIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,20 @@ public class cutsceneScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DialogText.text = cutsceneDialog[DialogIndex];
+    }
+    public void SelectDialog()
+    {
+        Debug.Log("test");
+        DialogText.gameObject.SetActive(true);
+        if (DialogIndex < cutsceneDialog.Count)
+        {
+            DialogIndex++;
+        }
+        else
+        {
+            //add stuff for once all dialog is finished
+        }
         
-    } 
+    }
 }
