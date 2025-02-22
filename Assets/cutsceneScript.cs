101196cb3d63bcea6e492fc95a3dd9a4c95d6ffd
kyafuso7 <70420682+kyafuso7@users.noreply.github.com>
2025-02-21 21:16:17 -0500

    Fixed merge conflict (missing cutsceneScript.cs)

---
+++ Assets/cutsceneScript.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class cutsceneScript : MonoBehaviour
+{
+    List<string> CadetDialog = new List<string>()
+    {
+        "",
+        "Hi, my name is Cadet",
+        "I like ube cupcakes.",
+
+    };
+
+
+    List<List<string>> CorporalDialog = new List<List<string>>()
+    {
+           new List<string>()
+         {
+             "",
+             "I'm a nice dude.",
+             "I'm a neutral dude.",
+             "I'm an asshole.",
+
+         },
+
+            new List<string>()
+         {
+             "",
+             "Oh, yeah? I like plums.",
+             "Lame.",
+             "",
+
+         },
+
+
+    };
+
+
+    public TextMeshProUGUI CadetDialogText;
+    int CadetDialogIndex;
+
+    public TextMeshProUGUI CorporalDialogOption1;
+    public TextMeshProUGUI CorporalDialogOption2;
+    public TextMeshProUGUI CorporalDialogOption3;
+    int CorporalDialogOptionIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CadetDialogText.text = CadetDialog[CadetDialogIndex];
+
+    }
+    public void SelectDialog()
+    {
+      
+        if (CadetDialogIndex < CadetDialog.Count-1)
+        {
+            CadetDialogText.gameObject.SetActive(true);
+            CadetDialogIndex++;
+            CorporalDialogOption1.gameObject.SetActive(false);
+            CorporalDialogOption2.gameObject.SetActive(false);
+            CorporalDialogOption3.gameObject.SetActive(false);
+        }
+        else
+        {
+            //add stuff for once all dialog is finished
+        }
+
+    }
+    public void NextDialog()
+    {
+        if (CorporalDialogOptionIndex < CorporalDialog.Count - 1)
+        {
+            CadetDialogText.gameObject.SetActive(false);
+            CorporalDialogOptionIndex++;
+            CorporalDialogOption1.gameObject.SetActive(true);
+            CorporalDialogOption2.gameObject.SetActive(true);
+            CorporalDialogOption3.gameObject.SetActive(true);
+        }
+    }
+
+}
