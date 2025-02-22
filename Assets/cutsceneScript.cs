31298378cf13bff776442b71ac6ffaf65533b253
abusch <abusch@umd.edu>
2025-02-21 19:48:39 -0500

    added list of lists for corporal dialogue

--- Assets/cutsceneScript.cs
+++ Assets/cutsceneScript.cs
@@ -5,20 +5,46 @@ using TMPro;
 
 public class cutsceneScript : MonoBehaviour
 {
-     List<string> cutsceneDialog = new List<string>()
+    List<string> CadetDialog = new List<string>()
     {
         "",
-        "testString1",
-        "testString2",
-       
+        "Hi, my name is Cadet",
+        "I like ube cupcakes.",
+
     };
+
+
+    List<List<string>> CorporalDialog = new List<List<string>>()
+    {
+           new List<string>()
+         {
+             "",
+             "I'm a nice dude.",
+             "I'm a neutral dude.",
+             "I'm an asshole.",
+
+         },
+
+            new List<string>()
+         {
+             "",
+             "Oh, yeah? I like plums.",
+             "Lame.",
+             "",
+
+         },
+
+
+    };
+
+
     public TextMeshProUGUI DialogText;
     int DialogIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -38,6 +64,6 @@ public class cutsceneScript : MonoBehaviour
         {
             //add stuff for once all dialog is finished
         }
-        
+
     }
 }
