eeff2f1f72987792d2995517a8ff622836b924f6
kyafuso7 <70420682+kyafuso7@users.noreply.github.com>
2025-03-13 20:42:43 -0400

    Resized text "hit box" and added Monologue text box

--- Assets/cutsceneScript.cs
+++ Assets/cutsceneScript.cs
@@ -30,9 +30,13 @@ public class cutsceneScript : MonoBehaviour {
     {
            new List<string>()
          {
-             "Um ...",
-             "",
-             "",
+
+               "The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown foxasdf",
+               "",
+               ""
+               //"Um ...",
+             //"",
+             //"",
        
          },
 
