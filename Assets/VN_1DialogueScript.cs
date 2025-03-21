704a1d2fb0f72977e49be5bcf08c20c7796b4476
kyafuso7 <70420682+kyafuso7@users.noreply.github.com>
2025-03-21 19:39:39 -0400

    Updated cutsceneScript
    Updated dialog to pull from VN_1DialogueScript and perform speaker detection.
    
    Updated VN_1DialogueScript to have line1, line2, line3 so that multiple dialog options are available.
    
    Updated dialog options 1-3 to turn off if there is no corresponding dialog for the respective option.

--- Assets/VN_1DialogueScript.cs
+++ Assets/VN_1DialogueScript.cs
@@ -7,7 +7,9 @@ public class VN_1DialogueScript : MonoBehaviour
 {
     public class DialogueLine
     {
-        public string line;
+        public string line1;
+        public string line2;
+        public string line3;
         public string speaker;
         public bool isMonologue; 
     }
@@ -15,18 +17,33 @@ public class VN_1DialogueScript : MonoBehaviour
     {
       new DialogueLine()
       {
-          line = "",
+          line1 = "Um...",
+          line2 = "",
+          line3 = "",
           speaker = "Corporal",
-          isMonologue = true,
+          isMonologue = false,
+      },
+       new DialogueLine()
+      {
+          line1 = "Well, technically it?s not satellite morning. Back at the Nairobi station it?s 7:27 am, though! And I don?t know about you, Corporal, but I?m still on Nairobi time!",
+          line2 = "",
+          line3 = "",
+          speaker = "Cadet",
+          isMonologue = false,
+      },
+        new DialogueLine()
+      {
+          line1 = "Look, um, I don't know how to say this but ... Have we met before?",
+          line2 =  "Look, um, I don't know how to say this but ... Who the hell are you?",
+          line3 = "",
+          speaker = "Corporal",
+          isMonologue = false,
       },
 
-    };   
-    public DialogueLine dialogueline = new DialogueLine()
-    {
-        line = "Well, technically it’s not satellite morning. Back at the Nairobi station it’s 7:27 am, though! And I don’t know about you, Corporal, but I’m still on Nairobi time!",
-        speaker = "Cadet",
+
     };
-    // Start is called before the first frame update
+
+    //// Start is called before the first frame update
     void Start()
     {
         
--- Assets/cutsceneScript.cs
+++ Assets/cutsceneScript.cs
@@ -6,131 +6,24 @@ using TMPro;
 public class cutsceneScript : MonoBehaviour { 
 
     // Scene #2 (Visual Novel Scene #1)
-     List<string> CadetDialog = new List<string>()
-    {
-        "",
-        "Well, technically it’s not satellite morning. Back at the Nairobi station it’s 7:27 am, though! And I don’t know about you, Corporal, but I’m still on Nairobi time!",
-        "Would you like some juice?",
-         "More for me, then! Are you more of a tea person?",
-         "Senior Cadet Aimee Kennington, Beta Branch 257!", 
-         "Usually, yes. But you hired me for the summer!",
-         "Whoa, we hit another jump point!",
-         "Good thing you bailed on the juice. I guess tequila shots followed by hyperspace is too much even for a vet.",
-        "The orange flecks are paint. Mostly.",
-        "No, no, no. You said you’d give me career advice for free, but that to be pleasant about it you’d need a shot.", 
-        "We got to talking and realized I’d actually applied for an internship at your outpost!", 
-        "That’s what you said last night!", 
-        "You said that too! Well, your actual words were \"None of these daisy-eyed grasshoppers are gonna last a month anyway before they crack like the outer hull on the Europa 17, but at least that chick looks like she can handle her liquor. I’ll hire her.\"", 
-        "Why did I accept an unpaid internship with an underfunded post office on the Outer Rim?", 
-        "That’ll just have to remain a mystery for now, won’t it? I will say this, though. I’m looking forward to changing the universe together, Corporal. One day at a time.",
-    };
-
-
-     List<List<string>> CorporalDialog = new List<List<string>>() 
-    {
-           new List<string>()
-         {
-             "Um ...",
-             "",
-             "",
-       
-         },
-
-              new List<string>()
-         {
-             "Look, um, I don't know how to say this but ... Have we met before?",
-             "Look, um, I don't know how to say this but ... Who the hell are you?",
-             "",
-
-         },
-
-                 new List<string>()
-         {
-             "Um. Shouldn't you be planetside, then? Taking classes?",
-             " ",
-             " ",
-
-         },
-            new List<string>()
-         {
-             "No, thanks.",
-             "If I swallow anything, I'll vomit.",
-             " ",
-
-         },
-
-            new List<string>()
-         {
-             "Right, tequila …. Okay, it’s coming back to me. We were at the grad ceremony …. My old squadmates and I decided to get drinks after … We walked into that place with the rusty door ….. ",
-             "",
-             "",
-
-         },
-
-           new List<string>()
-            {
-             "Right. My buddy Horaka and I saw a bunch of you dancing and bought drinks for everyone … we all started talking …. I said I’d give out career advice in exchange for free shots …. ",
-             "",
-             "",
-       
-         },
-
-           new List<string>()
-         {
-             "Okay, that checks out. And then, er…. I’m actually drawing a blank here …. ",
-             "",
-             "",
-       
-         },
-
-        new List<string>()
-           {
-             "Er. You did?  ",
-             "",
-             "",
-       
-         },
-          new List<string>()
-           {
-             "Erm. Look, I’ve been really busy, didn’t have time to read all the cover letters … ",
-             "",
-             "",
-       
-         },
-            new List<string>()
-           {
-             "…. Okay, it’s coming back. Look, I have a lot of questions, but there’s one I really want to get out of the way.",
-             "",
-             "",
-       
-         },
-               new List<string>()
-           {
-             "Yes. That.",
-             "Did we do anything, uh … did we do anything else last night?",
-             "Can you possibly forgive me?",
-       
-         },
-
-    };
-
     bool unveiling_text;
     int remaining_characters_to_unveil;
 
 
     public TextMeshProUGUI CadetDialogText;
-    int CadetDialogIndex;
 
     public GameObject CorporalTextPanel;
     public GameObject CadetTextPanel;
     public TextMeshProUGUI CorporalDialogOption1;
     public TextMeshProUGUI CorporalDialogOption2;
     public TextMeshProUGUI CorporalDialogOption3;
+    public TextMeshProUGUI MonologueDialog;
+    public VN_1DialogueScript ds;
     mouseOverScript CorporalDialogOptionColor1;
     mouseOverScript CorporalDialogOptionColor2;
     mouseOverScript CorporalDialogOptionColor3;
 
-    int CorporalDialogOptionIndex;
+    int dialogOptionIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -138,32 +31,50 @@ public class cutsceneScript : MonoBehaviour {
         CorporalDialogOptionColor1 = CorporalDialogOption1.gameObject.GetComponent<mouseOverScript>();
         CorporalDialogOptionColor2 = CorporalDialogOption2.gameObject.GetComponent<mouseOverScript>();
         CorporalDialogOptionColor3 = CorporalDialogOption3.gameObject.GetComponent<mouseOverScript>();
+        //MonologueDialog = GameObject.FindGameObjectWithTag("monologueButton").GetComponent<TextMeshProUGUI>();
+        ds = FindObjectOfType<VN_1DialogueScript>();
+
+        Debug.Log(ds.VNscene_1_dialogue[dialogOptionIndex]);
+        Debug.Log(ds.VNscene_1_dialogue[dialogOptionIndex].speaker);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        clickHandler();
-        Debug.Log(CadetDialogIndex);
-
-
-
-        if (!unveiling_text)
-        {
-            CadetDialogText.text = CadetDialog[CadetDialogIndex];
-        }
-        else
-        {
-
-            CadetDialogText.text = CadetDialog[CadetDialogIndex].Substring(0, CadetDialog[CadetDialogIndex].Length - remaining_characters_to_unveil);        
-        }
         
+        clickHandler();
+
+        VN_1DialogueScript.DialogueLine currentDialogLine = ds.VNscene_1_dialogue[dialogOptionIndex];
+        if (currentDialogLine.speaker == "Corporal")
+        {
+            CorporalDialogOption1.gameObject.SetActive(!string.IsNullOrEmpty(currentDialogLine.line1));
+            CorporalDialogOption2.gameObject.SetActive(!string.IsNullOrEmpty(currentDialogLine.line2));
+            CorporalDialogOption3.gameObject.SetActive(!string.IsNullOrEmpty(currentDialogLine.line3));
+            CorporalDialogOption1.text = currentDialogLine.line1;
+            CorporalDialogOption2.text = currentDialogLine.line2;
+            CorporalDialogOption3.text = currentDialogLine.line3;
+        }
+
+        else if (currentDialogLine.speaker == "Cadet")
+        {
+            if (!unveiling_text)
+            {
+                CadetDialogText.text = currentDialogLine.line1;
+            }
+            else
+            {
+
+                CadetDialogText.text = currentDialogLine.line1.Substring(0, currentDialogLine.line1.Length - remaining_characters_to_unveil);
+            }
+        }
 
 
 
-        CorporalDialogOption1.text = CorporalDialog[CorporalDialogOptionIndex][0];
-        CorporalDialogOption2.text = CorporalDialog[CorporalDialogOptionIndex][1];
-        CorporalDialogOption3.text = CorporalDialog[CorporalDialogOptionIndex][2];
+
+
+
+        
 
     }
 
@@ -204,19 +115,19 @@ public class cutsceneScript : MonoBehaviour {
     public void SelectDialog()
     {
       
-        if (CadetDialogIndex < CadetDialog.Count-1)
+        if (dialogOptionIndex < ds.VNscene_1_dialogue.Count-1)
         {
             CorporalTextPanel.gameObject.SetActive(false);
 
             CadetTextPanel.gameObject.SetActive(true);
             CadetDialogText.gameObject.SetActive(true);
-            CadetDialogIndex++;
+            dialogOptionIndex++;
             CorporalDialogOption1.gameObject.SetActive(false);
             CorporalDialogOption2.gameObject.SetActive(false);
             CorporalDialogOption3.gameObject.SetActive(false);
 
             unveiling_text = true;
-            remaining_characters_to_unveil = CadetDialog[CadetDialogIndex].Length;
+            remaining_characters_to_unveil = ds.VNscene_1_dialogue[dialogOptionIndex].line1.Length;
             StartCoroutine(unveil_letter());
         }
         else
@@ -227,13 +138,13 @@ public class cutsceneScript : MonoBehaviour {
     }
     public void NextDialog()
     {
-        if (CorporalDialogOptionIndex < CorporalDialog.Count - 1)
+        if (dialogOptionIndex < ds.VNscene_1_dialogue.Count - 1)
         {
             CorporalTextPanel.gameObject.SetActive(true);
             CadetTextPanel.gameObject.SetActive(false);
 
             CadetDialogText.gameObject.SetActive(false);
-            CorporalDialogOptionIndex++;
+            dialogOptionIndex++;
 
             CorporalDialogOption1.gameObject.SetActive(true);
             CorporalDialogOption2.gameObject.SetActive(true);
